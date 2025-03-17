945e06dc40ea8b3880eed036e8e7b748cacefd8e
Proxtron <51761303+Proxtron@users.noreply.github.com>
2025-03-17 12:06:31 -0700

    Add input to change # of squares and input validation

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const numberOfDivs = 512;
-const numberSqrt = Math.sqrt(numberOfDivs);
 const containerDiv = document.querySelector(".container");
 
-function createGrid() {
-    for(let i = 0; i < numberSqrt; i++) {
+function createGrid(numberPerSide) {
+    containerDiv.innerHTML = "";
+    for(let i = 0; i < numberPerSide; i++) {
         const rowDiv = document.createElement("div");
         rowDiv.setAttribute("class", "row");
-        for(let j = 0; j < numberSqrt; j++) {
+        for(let j = 0; j < numberPerSide; j++) {
             const column = document.createElement("div");
             column.setAttribute("class", "square");
             rowDiv.appendChild(column);
@@ -15,7 +14,27 @@ function createGrid() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", createGrid);
-document.querySelector(".container").addEventListener("mouseover", (event) => {
-    event.target.classList.add("black");
+function setSquares() {
+    const input = parseInt(prompt("Set # of square per side (Max: 100)"));
+    if(input > 100) {
+        document.querySelector(".error-div").style.display = "block";
+        document.querySelector(".error-div").innerText = "# of squares per side must be under 100";
+    } else if(isNaN(input) || input < 1) {
+        document.querySelector(".error-div").style.display = "block";
+        document.querySelector(".error-div").innerText = "Must input a valid number";
+    } else {
+        document.querySelector(".error-div").style.display = "none";
+        createGrid(input);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    createGrid(20);
+    document.querySelector(".container").addEventListener("mouseover", (event) => {
+        if(event.currentTarget != event.target) {
+            event.target.classList.add("black");
+        }
+    });
+
+    document.querySelector("#setSquaresBtn").addEventListener("click", setSquares);
 });
